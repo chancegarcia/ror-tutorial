67cce069c0d668c4f7eb754389781ac1db229a76
chance garcia <develop@chancegarcia.com>
2012-04-16 15:07:27 -0400

    files after the generate controller step in the tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd20b04ef069e6b69b4665987c0664fc732dd47d25fbf035a3d0e666165485e6d6cda839cefc66cd61d4733468060a95322151db14ce3f324a1402c333aa1c15e'
